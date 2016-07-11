928c478fa90d3f6f4564fde9dfe1c213d7aa006c
Deyear <1207457319@qq.com>
2016-07-11 09:51:11 +0800

    Initial Commit

---
+++ ShareDemoQQAndWeixin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ShareDemoQQAndWeixin
+//
+//  Created by 牛牛 on 16/7/11.
+//  Copyright © 2016年 HanghuanTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
